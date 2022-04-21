439a7839f13d1487034a11aa9b703c7b7e3e27db
Mirosław Potrykus <86200105+Mirolg@users.noreply.github.com>
2022-04-21 13:07:06 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const price = document.querySelector('#price');
+const people = document.querySelector('#people');
+const tip = document.querySelector('#tip');
+const error = document.querySelector('.error');
+const countBtn = document.querySelector('.count');
+const costInfo = document.querySelector('.cost-info');
+const cost = document.querySelector('.cost');
+
+function checkBill() {
+	if (price.value === '' || people.value === '' || tip.value === 0) {
+		error.textContent = 'Zapomniałeś uzupełnić wszystkie pola!';
+		costInfo.style.display = 'none';
+	} else {
+		error.textContent = '';
+		showBill();
+	}
+}
+function showBill() {
+	const sum =
+		(parseFloat(price.value) + parseInt(price.value) * parseFloat(tip.value)) /
+		parseInt(people.value);
+
+	costInfo.style.display = 'block';
+	cost.textContent = sum.toFixed(2);
+}
+
+countBtn.addEventListener('click', checkBill);
